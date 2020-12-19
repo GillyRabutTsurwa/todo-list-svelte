15af1ec50e2739b5a6f911a345eaa0873c25d3d6
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-19 17:30:08 -0600

    added sass to my project

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,8 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
+<style type="text/scss">
+	$color: pink;
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,7 +12,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: $color;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
@@ -28,3 +24,12 @@
 		}
 	}
 </style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
